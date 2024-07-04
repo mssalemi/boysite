b51677a092634ab10bd386c4b1126209c80d2512
Mehdi Salemi <mehdisalemi@Mehdis-MacBook-Air.local>
2024-07-04 11:27:12 -0400

    updatez

--- app/components/CurrentPlan.tsx
+++ app/components/CurrentPlan.tsx
@@ -1,15 +1,22 @@
 import React from "react";
-import { Banner, Text, List } from "@shopify/polaris";
+import { Banner, Text, List, Link, Tooltip } from "@shopify/polaris";
 
 export function CurrentPlan() {
   return (
     <Banner title="Current Plan" tone="info" onDismiss={() => {}}>
       <div className="space-y-2">
         <Text variant="bodyMd" as="p">
-          <strong>Location:</strong> Somewhere near Toronto - Muskoka? - PEC -
-          Ottawa if that's easy rent a big place to fit everybody and we're not
-          tight - although in true Mehdi yolo fashion, I'm doing this last
-          minute.
+          <Tooltip content="TBD - looking for big ass airbnb close to golf spots">
+            <strong>Location:</strong>{" "}
+          </Tooltip>
+          Somewhere near Toronto and rent a big place to fit everybody and we're
+          not tight - although in true Mehdi yolo fashion, I'm doing this last
+          minute. Golf courses im thinking are Glen Abbey and TPC Toronto. Was
+          thinking Padel/Pickelball too but dont think I can jam it all in one
+          day.
+        </Text>
+        <Text variant="bodyMd" as="p">
+          <strong>Location:</strong> Somewhere in Toronto/Mississauga
         </Text>
         <Text variant="bodyMd" as="p">
           <strong>Days:</strong> Aug 23 - Aug 25
@@ -19,29 +26,35 @@ export function CurrentPlan() {
           and do some fun activities ❤️
         </Text>
         <Text variant="bodyMd" as="p">
-          <strong>Activities:</strong>
+          <strong>Mehdi's Bachelor Weekend:</strong>
         </Text>
         <List type="bullet">
-          <List.Item>Friday: Arrive - BBQ Dinner at Airbnb and chill</List.Item>
+          <List.Item>Friday: Arrive - BBQ Dinner at Airbnb. </List.Item>
           <List.Item>
             Saturday:
             <List type="bullet">
-              <List.Item>Maybe bench in morning lol</List.Item>
+              <List.Item>Early Morning Bench Workout</List.Item>
               <List.Item>
-                Golf 9-10ish am tee off (Have lunch there after, prob finish
-                3pm){" "}
+                Golf 10ish am tee off at{" "}
+                <Link url="https://glenabbey.clublink.ca/about/gallery/">
+                  Glen Abbey
+                </Link>{" "}
+                (Have lunch there after, prob home by 4)
               </List.Item>
               <List.Item>
-                Go to base camp, shower, get ready for padel Tournament
+                Go to base camp, shower, do a lil pre drink for dinner, get a
+                corn hole going.
               </List.Item>
-              <List.Item>Padel 5-7pm</List.Item>
-              <List.Item>Post padel drinks at Airbnb</List.Item>
               <List.Item>Dinner</List.Item>
-              <List.Item>Chess / Cornhole at night</List.Item>
+              <List.Item>Dinner: Chill and Chess</List.Item>
             </List>
           </List.Item>
           <List.Item>
-            Sunday: Early Golf (if you want) - brunch and pce
+            Sunday: Early Golf at{" "}
+            <Link url="https://www.ospreyvalley.com/hoot">
+              TPC Toronto, The Hoot
+            </Link>{" "}
+            if you want and pce
           </List.Item>
         </List>
       </div>
